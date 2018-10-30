d4d301b427bd8240b7f4f012023564f7ce10291b
lei.dai <lei.dai@dragonflow.com>
2018-10-30 13:49:37 +0800

    [mqtt-ctrl-m/vppnctrl]:remove useless files

--- git_home/mqtt-ctrl-m/vppnctrl/delete_files/vppnconfig/bird_conf.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BIRD_CONF_H__
-#define __BIRD_CONF_H__
-
-int set_bird_file();
-
-#endif
--- git_home/mqtt-ctrl-m/vppnctrl/delete_files/vppnconfig/nvram-op.h
+++
@@ -1,8 +0,0 @@
-#ifndef __NVRAM_OP_H__
-#define __NVRAM_OP_H__
-
-void SetConfig(const char *name, char *value);
-void UnsetConfig(const char *name);
-char* GetConfig(const char *name);
-
-#endif
