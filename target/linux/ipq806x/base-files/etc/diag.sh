bc590505b191fa659dd061259b598cc48dbfaec0
jing.li <jing.li@dragonflow.com>
2018-11-06 15:20:25 +0800

    [target] fix bootup system led action

---
+++ target/linux/ipq806x/base-files/etc/diag.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Copyright (C) 2006-2009 OpenWrt.org
+
+set_state() {
+    case "$1" in
+    preinit)
+        insmod leds-gpio
+        insmod ledtrig-default-on
+        insmod ledtrig-timer
+        ledcontrol -n power -c amber -s blink
+        ;;
+    failsafe)
+        ledcontrol -n power -c amber -s fast_blink
+        ;;
+    done)
+        ledcontrol -n power -c green -s on
+        ;;
+    esac
+}
